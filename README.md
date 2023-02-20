304a301d96d493274d51286f5fdad0e093474d26
SergY29 <yurtin.sergey.v@gmail.com>
2023-02-20 12:54:38 +0200

    finish

---
+++ README.md
@@ -0,0 +1,6 @@
+1. https://monosnap.com/file/NtEYFijgAQsjpDOPDolir1NP5EwTeh
+2. https://monosnap.com/file/33OwTralUl1p8gxaQfzWdD0Z3FyLbH
+3. https://monosnap.com/file/27vtuDhrb476FImX5VSU0opp4CWVwL
+4. https://monosnap.com/file/lSpJlKUsJggYaiTVAptngrgNia8gJ0
+
+repository: https://github.com/SergY29/Node_CLI_-HW_1
--- contacts.js
+++ contacts.js
@@ -6,40 +6,59 @@ const { v4: uuidv4 } = require('uuid');
 const contactsPath = path.resolve('./db/contacts.json')
 
 const listContacts = async () => {
-    const dataString = await fs.readFile(contactsPath, 'utf8');
-    const data = JSON.parse(dataString);
-    return data;
+    try {
+        const dataString = await fs.readFile(contactsPath, 'utf8');
+        const data = JSON.parse(dataString);
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+
 }
 
 
 const getContactById = async (contactId) => {
-    const contacts = await listContacts();
-    const contact = contacts.filter(contact => contact.id === String(contactId));
-    return console.log(contact);
+    try {
+        const contacts = await listContacts();
+        const contact = contacts.filter(contact => contact.id === String(contactId));
+        return console.log(contact);
+    } catch (error) {
+        console.log(error)
+    }
+
 }
 
 const removeContact = async (contactId) => {
-    const contacts = await listContacts();
-    const idx = contacts.findIndex(contact => contact.id === String(contactId));
-    const deleteContact = contacts[idx];
+    try {
+        const contacts = await listContacts();
+        const idx = contacts.findIndex(contact => contact.id === String(contactId));
+        const deleteContact = contacts[idx];
 
-    contacts.splice(idx, 1);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts), 'utf8');
-    return console.log(deleteContact);
+        contacts.splice(idx, 1);
+        await fs.writeFile(contactsPath, JSON.stringify(contacts), 'utf8');
+        return console.log(deleteContact);
+    } catch (error) {
+        console.log(error)
+    }
+
 }
 
 const addContact = async (name, email, phone) => {
-
-    const contacts = await listContacts();
     const newContact = {
         id: uuidv4(),
         name,
         email,
         phone,
     };
-    const newListContacts = [...contacts, newContact];
-    await fs.writeFile(contactsPath, JSON.stringify(newListContacts), 'utf8');
-    return console.log(newContact)
+
+    try {
+        const contacts = await listContacts();
+        const newListContacts = [...contacts, newContact];
+        await fs.writeFile(contactsPath, JSON.stringify(newListContacts), 'utf8');
+        return console.log(newContact)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 module.exports = {
